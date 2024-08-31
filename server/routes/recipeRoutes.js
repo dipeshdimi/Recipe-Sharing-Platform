d322927c18fa468cb5747e2e3c3f961176596650
Dipesh Mishra <dipeshdimi@gmail.com>
2024-09-01 02:13:34 +0530

    routes update

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -19,7 +19,5 @@ const upload = multer({ storage });
 router.get("/", recipeController.getRecipes);
 router.get("/:id", recipeController.getRecipeById);
 router.post("/", upload.single("image"), recipeController.createRecipe);
-router.put("/:id", upload.single("image"), recipeController.updateRecipe);
-router.delete("/:id", recipeController.deleteRecipe);
 
 module.exports = router;
